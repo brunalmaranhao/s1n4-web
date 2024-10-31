40194cbaf90381644355229d1199cd397f9b9b46
Luiz de Freitas <luizdefreitas@MacBook-Pro-de-Luiz.local>
2024-10-31 09:42:17 -0300

    feat: dark mode manage client screen

--- src/components/HeaderManagement/HeaderManagement.tsx
+++ src/components/HeaderManagement/HeaderManagement.tsx
@@ -1,12 +1,18 @@
 "use client";
-import { Button } from "@nextui-org/react";
+import { Button, Switch } from "@nextui-org/react";
 import { usePathname, useRouter } from "next/navigation";
 import TabsManagement from "./TabsManagement/TabsManagement";
 import { useProjectContext } from "@/context/ProjectContext";
 import FilterProjectsByCustomer from "../FilterProjectsByCustomer/FilterProjectsByCustomer";
 import Notification from "../Notification/Notification";
+import { SunIcon } from "../SunIcon/SunIcon";
+import { MoonIcon } from "../MoonIcon/MoonIcon";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { GrAdd } from "react-icons/gr";
 
 export default function HeaderManagement() {
+  const { theme, setTheme } = useTheme();
   const { onOpen } = useProjectContext();
   const { push } = useRouter();
   const pathsWithTab = [
@@ -19,13 +25,25 @@ export default function HeaderManagement() {
     : "Projetos";
   return (
     <div className="flex flex-col w-full gap-6">
-      <div className="flex flex-row justify-between w-full">
-        <h1 className="text-[42px] text-[#21272A] font-bold">
+      <div className="flex flex-row justify-between mt-4 items-center w-full">
+        <h1 className="text-[42px] text-[#21272A] dark:text-white font-bold">
           Gerenciar - {label}{" "}
         </h1>
-        <Notification />
+        <div className="flex flex-row gap-4 items-center">
+          <Switch
+            defaultSelected
+            size="lg"
+            startContent={<SunIcon />}
+            endContent={<MoonIcon />}
+            onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+            classNames={{
+              wrapper: "group-data-[selected=true]:bg-[#F57B00]",
+            }}
+          ></Switch>
+          <Notification />
+        </div>
       </div>
-      <div className="flex flex-row justify-between w-full flex-wrap gap-6">
+      <div className="flex flex-row justify-between items-center w-full flex-wrap gap-6">
         {pathsWithTab.includes(pathname) && <TabsManagement />}
 
         {label === "Clientes" && pathsWithTab.includes(pathname) && (
@@ -33,6 +51,8 @@ export default function HeaderManagement() {
             <Button
               color="primary"
               onPress={() => push("/admin/management/customers/new")}
+              startContent={<GrAdd />}
+              className="pr-6 bg-[#F57B00]"
             >
               Novo Cliente
             </Button>
