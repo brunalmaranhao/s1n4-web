c48821deeac9620750432419acb6719f4f504e0f
Yuri Muniz <y.munizgonzalez@gmail.com>
2024-11-12 10:42:18 -0300

    new layout login

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "../globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "Login - Grupo Sina",
+  description: "Grupo Sina",
+};
+
+export default function LoginLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <div className="flex w-full min-h-screen">{children}</div>
+    </div>
+  );
+}
