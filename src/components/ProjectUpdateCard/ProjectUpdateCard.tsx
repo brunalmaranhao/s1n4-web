9da9b0f32e11a61ace3db9ca812731eca774cb0e
Luiz de Freitas <luizdefreitas@MacBook-Pro-de-Luiz.local>
2024-12-06 12:08:54 -0300

    fix: lint

--- src/components/ProjectUpdateCard/ProjectUpdateCard.tsx
+++ src/components/ProjectUpdateCard/ProjectUpdateCard.tsx
@@ -55,7 +55,7 @@ export default function ProjectUpdateCard({
         </div>
       </div>
       <h1 className="text-black dark:text-white py-2">
-        &quot;{projectUpdate.description}&quot;
+        {projectUpdate.description};
       </h1>
       {isLast ? null : <hr className="border-[#878D9633]" />}
     </div>
