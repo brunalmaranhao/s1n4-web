a784eb9bfe4fbcce9fb1b1ec33363d2976c9772f
Yuri Muniz <y.munizgonzalez@gmail.com>
2025-01-09 10:07:08 -0300

    comments-user-customer

--- src/components/ProjectsComponent/ProjectUpdateComponent/ProjectUpdateComponent.tsx
+++ src/components/ProjectsComponent/ProjectUpdateComponent/ProjectUpdateComponent.tsx
@@ -188,7 +188,11 @@ export default function ProjectUpdateComponent() {
                 </div>
               </form>
             ) : (
-              <p className="text-[16px] font-normal my-1">
+              <p className="text-[16px] font-normal my-1 w-full overflow-auto [&::-webkit-scrollbar]:h-2
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
                 {projectUpdate.description}
               </p>
             )}
